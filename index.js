82d4390806cfa2e4b9f6ebc3b7e60ef62e214312
GayathriSwethaDara <swethadvg23@outlook.com>
2024-07-16 15:29:56 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT=8000;
 
 //initialize app
 const app=express();
 
+//connectiong to db
+const connectionUrl = "mongodb://localhost:27017/todoDb";
+mongoose.connect(connectionUrl).then(() => console.log("database connected successfully")).catch((error) => console.log(error.message));
+
 //view engine
 app.set("view engine","ejs");
+app.get("/",(req,res,next)=>{
+    try{
+        res.render("index");
+    } catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
 
 //listen server
 app.listen(PORT, () =>{
